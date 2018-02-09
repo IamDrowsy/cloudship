15a92d1c39c7d432fc21d50bbdbda52f54cbfcda
Albrecht Schmidt <Albrecht.Schmidt@t-systems.com>
2018-02-09 16:32:45 +0100

    assert-input now removes a list of secret keys.

--- src/cloudship/util/spec.clj
+++ src/cloudship/util/spec.clj
@@ -1,11 +1,22 @@
 (ns cloudship.util.spec
   (:require [clojure.spec.alpha :as s]
+            [cloudship.connection.props.spec :as cs]
             [expound.alpha :refer [expound-str]]))
+
+(def secret-keys [::cs/password ::cs/kppass :password :kppass])
+
+(defn obscure-key [m key]
+  (if (key m)
+    (assoc m key "*******")
+    m))
+
+(defn obscure-keys [m keys]
+  (reduce obscure-key m keys))
 
 (defn assert-input [spec input]
   (if (not (s/valid? spec input))
-    (throw (ex-info (expound-str spec input)
+    (throw (ex-info (expound-str spec (obscure-keys input secret-keys))
                     {:spec spec
-                     :input input}))
+                     :input (obscure-keys input secret-keys)}))
     input))
 
