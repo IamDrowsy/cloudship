7ea541ddf87d3c8c9c6e8ee2eab8e8b431bcc814
Albrecht Schmidt <mail@albrechtschmidt.de>
2018-10-12 22:08:07 +0200

    switched from swt browser to web server flow. this is way better on the dependency side and platform independent

--- src/cloudship/util/web_auth.clj
+++ src/cloudship/util/web_auth.clj
@@ -1,11 +1,15 @@
 (ns cloudship.util.web-auth
   (:require [clojure.string :as str]
             [com.rpl.specter :refer :all]
-            [clj-http.util :as u])
-  (:import (org.eclipse.swt.browser Browser)
-           (org.eclipse.swt.widgets Display Shell)
-           (org.eclipse.swt SWT)
-           (org.eclipse.swt.layout FillLayout)))
+            [clj-http.util :as u]
+            [clj-http.client :as http]
+            [cheshire.core :as json]
+            [ring.middleware.params :as params]
+            [org.httpkit.server :as server]
+            [taoensso.timbre :as t])
+  (:import (java.util.concurrent ArrayBlockingQueue TimeUnit)
+           (java.awt Desktop)
+           (java.net URI)))
 
 (defn check-and-parse-callback [current-url options]
   (when (and (str/starts-with? current-url (:callback-url options))
@@ -18,49 +22,111 @@
           (into {} u)
           (transform [MAP-KEYS] keyword u))))
 
-(defn gui-loop [browser display shell options]
-  (loop []
-    (if (.isDisposed shell)
-      (.dispose display)
-      (do
-        (if-not (.readAndDispatch display)
-          (.sleep display))
-        (if-let [result (check-and-parse-callback (.getUrl browser) options)]
-          (do (.dispose display)
-              result)
-          (recur))))))
+(defonce server (atom nil))
+; holds all running request, keys are
+; :options, -> to original option map
+; :callback-queue, -> the queue for writing the final answer
+; :state -> one of #{:open, :got-code, :done}
+;  key is :cache-name or "default"
+(defonce open-requests (atom {}))
+
+(defn- queue-for-request ^ArrayBlockingQueue [request-key]
+  (:callback-queue (@open-requests request-key)))
+
+(defn- state-for-request [request-key]
+  (:state (@open-requests request-key)))
+
+(defn- options-for-request [request-key]
+  (:options (@open-requests request-key)))
+
+(defn- auth-url [base-url]
+  (str base-url "/services/oauth2/authorize"))
+
+(defn- token-url [base-url]
+  (str base-url "/services/oauth2/token"))
+
+(defn complete-web-server-flow [request-key code]
+  (let [{:keys [base-url consumer-key callback-url consumer-secret]} (options-for-request request-key)
+        optional (if consumer-secret {:client_secret consumer-secret} {})
+        auth-result (http/post (token-url base-url)
+                               {:form-params (merge {:grant_type   "authorization_code"
+                                                     :code         code
+                                                     :client_id    consumer-key
+                                                     :redirect_uri callback-url}
+                                                    optional)})]
+    (json/parse-string (:body auth-result) true)))
+
+(defn- handle-code-callback [request-key params]
+  (let [code (get params "code")]
+    (swap! open-requests assoc-in [request-key :state] :got-code)
+    (.put (queue-for-request request-key) (complete-web-server-flow request-key code))))
+
+(defn- handle-request [req]
+  (let [params (:params (params/params-request req))
+        request-key (params "state")]
+    (cond (= (state-for-request request-key) :open)
+          (handle-code-callback request-key params)
+          :else
+          {:status 200
+           :headers {"Content-Type" "text/html"}
+           :body "Success"})))
+
+(defn- stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn- start-server [options]
+  (when (nil? @server)
+    (reset! server (server/run-server #'handle-request {:port (:callback-port options 8080)}))))
 
 (def default-options
-  {:consumer-key "3MVG98_Psg5cppybVAyG0nED1FctTXymm0nXYgOZxeI2hSj3dKRhO6w3rtDgWchoNsOeu.4EzrezJRbJyW58C"
+  {:consumer-key #_"3MVG98_Psg5cppybVAyG0nED1FctTXymm0nXYgOZxeI2hSj3dKRhO6w3rtDgWchoNsOeu.4EzrezJRbJyW58C"
+   "3MVG98_Psg5cppyZCaw1PcLjYvU32k1LsZ8ZU76RP_vq5uycVvzL05cYgeQJ97bFsynhV94lXqWbc2Xdbzsex"
    :base-url "https://login.salesforce.com"
    :callback-url "http://localhost"
-   :response-type "token"})
+   :response-type "code"
+   :callback-port 80
+   :callback-timeout 60})
 
-(defn build-url [options]
+(defn- build-url [request-key options]
   (let [url
-        (str (str (:base-url options) "/services/oauth2/authorize")
+        (str (auth-url (:base-url options))
              "?response_type=" (u/url-encode (:response-type options))
              "&client_id=" (u/url-encode (:consumer-key options))
+             "&state=" (u/url-encode request-key)
+             ; we don't need the url here, as salesforce always redirects to the port configured in the connected app
              "&redirect_uri=" (u/url-encode (:callback-url options)))]
     url))
 
-(defn show-browser [options]
-  (let [display (Display.)
-        shell (Shell. display)
-        layout (FillLayout. SWT/VERTICAL)
-        browser (Browser. shell, SWT/NONE)]
-    (.setUrl browser (build-url options))
-    (doto shell
-      (.setText "cloudship Web Auth")
-      (.setLayout layout)
-      (.setSize 640 800)
-      (.pack)
-      (.open))
-    (gui-loop browser display shell options)))
+(defn- open-auth-url [request-key options]
+  (.browse (Desktop/getDesktop) (URI. (build-url request-key options))))
+
+(defn- await-callback [request-key options]
+  (let [answer (.poll (queue-for-request request-key) (:callback-timeout options) TimeUnit/SECONDS)]
+    (if (nil? answer)
+      (throw (ex-info "Requested Auth timed out." {}))
+      answer)))
+
+(defn- start-and-await-browser-callback [request-key options]
+  (start-server options)
+  (open-auth-url request-key options)
+  (let [result (await-callback request-key options)
+        remaining-requests (swap! open-requests dissoc request-key)]
+    (if (empty? remaining-requests)
+      (stop-server))
+    result))
+
+(defn- init-request-state [request-key options]
+  (swap! open-requests merge {request-key {:options options
+                                           :callback-queue (ArrayBlockingQueue. 1)
+                                           :state :open}}))
 
 (defn run-web-auth [options]
   (let [full-options (merge default-options options)
-        result (show-browser full-options)]
-    {:session (u/url-decode (:access_token result))
-     :url (u/url-decode (:instance_url result))
-     :refresh-token (u/url-decode (or (:refresh_token result) ""))}))
+        request-key (str (:cache-name options "default"))
+        _ (init-request-state request-key full-options)
+        result (start-and-await-browser-callback request-key full-options)]
+    {:session (:access_token result)
+     :url (:instance_url result)
+     :refresh-token  (or (:refresh_token result) "")}))
