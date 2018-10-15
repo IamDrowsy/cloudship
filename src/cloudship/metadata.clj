c1f05d672c2b445f4598e30b1b7a95029f534b76
Albrecht Schmidt <Albrecht.Schmidt@t-systems.com>
2018-10-15 10:12:07 +0200

    some docstring for metadata

--- src/cloudship/metadata.clj
+++ src/cloudship/metadata.clj
@@ -5,19 +5,29 @@
             [cloudship.client.core :as c]
             [cloudship.util.misc :as misc]))
 
-(defn describe [cloudship]
+(defn describe
+  "Returns the global meta describe data for the given cloudship."
+  [cloudship]
   (p/describe cloudship))
 
-(defn describe-type [cloudship type]
+(defn describe-type
+  "Returns the describe data for the given cloudship and metadata type"
+  [cloudship type]
   (p/describe-type cloudship type))
 
-(defn list [cloudship metadata-type]
+(defn list
+  "Returns a list of all existing entries of the given cloudship and metadata type"
+  [cloudship metadata-type]
   (p/list cloudship cloudship metadata-type))
 
-(defn read [cloudship metadata-type & metadata-names]
+(defn read
+  "Returns the metadata for the given metadata-names of the metadata type."
+  [cloudship metadata-type & metadata-names]
   (p/read cloudship cloudship metadata-type (misc/normalize-simple-var-args metadata-names)))
 
-(defn read-all [cloudship metadata-type]
+(defn read-all
+  "Returns the metadat for all metadata entries of the given metadata type."
+  [cloudship metadata-type]
   (p/read cloudship cloudship metadata-type (list cloudship metadata-type)))
 
 (defn update [cloudship metadata]
