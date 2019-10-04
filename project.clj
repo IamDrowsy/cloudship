2b6dafeb451af077492d028e713fade8b27d3071
Albrecht Schmidt <mail@albrechtschmidt.de>
2019-10-04 10:55:07 +0200

    Version ups with lein ancient

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/IamDrowsy/cloudship"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [instaparse "1.4.10"]
                  [semantic-csv "0.2.1-alpha1"]
                  [com.taoensso/timbre "4.10.0"]
@@ -12,11 +12,11 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/java.data "0.1.1"]
                  [clojure.java-time "0.3.2"]
-                 [org.clojure/core.cache "0.7.2"]
+                 [org.clojure/core.cache "0.8.2"]
                  [com.taoensso/nippy "2.14.0"]
                  [http-kit "2.3.0"]
                  [ring "1.7.1"]
-                 [cheshire "5.8.1"]
+                 [cheshire "5.9.0"]
                  [ebenbild "0.2.0"]
                  [com.rpl/specter "1.1.2"]
                  [clj-http "3.10.0"]
@@ -25,6 +25,6 @@
                  [com.force.api/force-metadata-api "47.0.0"]]
 
   :profiles {:dev {:resource-paths ["resources-test"]
-                   :dependencies [[org.clojure/test.check "0.10.0-alpha2"]
+                   :dependencies [[org.clojure/test.check "0.10.0"]
                                   [orchestra "2019.02.06-1"]
                                   [criterium "0.4.5"]]}})
