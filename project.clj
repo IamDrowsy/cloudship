4742c09e1333d04e8d2119d5c84092bcaeecb2d7
Albrecht Schmidt <mail@albrechtschmidt.de>
2019-11-12 00:38:13 +0100

    version up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloudship "0.2.1-SNAPSHOT"
+(defproject cloudship "0.2.1"
   :description "cloudship is a clojure toolkit to explore and manipulate your salesforce instances"
   :url "https://github.com/IamDrowsy/cloudship"
   :license {:name "Eclipse Public License"
